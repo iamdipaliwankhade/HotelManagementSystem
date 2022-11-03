613f9aab110d751baf9405e289a79c3e0e4b9b5c
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-11-03 11:56:45 +0530

    [Dipali] added: Welcome message

---
+++ src/com/hotelmanagement/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package com.hotelmanagement;
+
+public class HotelReservationSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation system");
+	}
+
+}
